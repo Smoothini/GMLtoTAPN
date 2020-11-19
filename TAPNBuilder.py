41bb8367b9c48b88ea92f7dbf2398239ab5b50fc
Smoothini <katona.andrei@gmail.com>
2020-11-19 10:53:27 +0100

    big nets 1000-5000

--- TAPNBuilder.py
+++ TAPNBuilder.py
@@ -63,5 +63,5 @@ def write_all_to_file():
 #write_to_file("btNorthAmerica")
 #write_to_file("Aconet")
 
-TN.write_batch_to_file(10, 100, 10)
+TN.write_batch_to_file(1000, 5000, 1000)
 #TN.make_shared(100)
