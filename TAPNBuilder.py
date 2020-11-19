bb90a4ac1c39a995a97b4de92cef20824be8558d
Smoothini <katona.andrei@gmail.com>
2020-11-19 08:54:39 +0100

    haha yes

--- TAPNBuilder.py
+++ TAPNBuilder.py
@@ -63,4 +63,5 @@ def write_all_to_file():
 #write_to_file("btNorthAmerica")
 #write_to_file("Aconet")
 
-TN.write_batch_to_file(100, 1100, 100)
+#TN.write_batch_to_file(100, 1100, 100)
+TN.make_disjoint(5000)
