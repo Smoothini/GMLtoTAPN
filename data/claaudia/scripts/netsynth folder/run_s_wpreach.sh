55f50daf24f58e6c90330a6f54f6ba1c5e0ee8a6
Smoothini <katona.andrei@gmail.com>
2020-12-08 17:07:30 +0100

    time interval added

---
+++ data/claaudia/scripts/netsynth folder/run_s_wpreach.sh
@@ -0,0 +1,8 @@
+echo "Batch 1: 200 to 1000 nodes"
+for i in 10 20 30 40 50 60 70 80 90 100 200 300 400 500 600 700 800 900 1000
+do
+	echo "Running Shared_$i.."
+	{ time timeout -k 5 5s ./netsynth solve ltl/Shared_$i.ltl ; } 2> ltl_results/Shared/Shared_$i.txt
+	echo "Done!"
+done
+echo "Done batch 1"
