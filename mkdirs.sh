c35f26c48bc19f19a225408993e24bd552307995
Smoothini <katona.andrei@gmail.com>
2020-12-28 04:54:47 +0100

    Readme and small fix

---
+++ mkdirs.sh
@@ -0,0 +1,18 @@
+mkdir data/synthethic_json
+mkdir data/synthethic_dtapn
+mkdir data/synthethic_ltl
+mkdir data/synthethic_tapaal
+mkdir data/synthethic_results
+
+mkdir data/zoo_json
+mkdir data/zoo_dtapn
+mkdir data/zoo_ltl
+mkdir data/zoo_tapaal
+mkdir data/zoo_results
+
+mkdir data/time
+mkdir data/time/Disjoint
+mkdir data/time/Shared
+mkdir data/time/Worst
+mkdir data/time/Zoo
+
